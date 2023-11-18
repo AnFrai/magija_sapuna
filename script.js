bcd64a5abc9e774e92eb9a4da6d3e452336d5227
Anastasia <an.gorskova.bg@gmail.com>
2023-11-18 13:51:14 +0100

    Заканчивает стилизацию хедера

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let headerCartButton = document.querySelector('.header-cart-button');
+  let productsNumber = document.querySelector('.products-number');
+  let productPlus = document.querySelector('.plus');
+  let productMinus = document.querySelector('.minus');
+  let numProducts = parseInt(productsNumber.textContent);
+
+  function updateCart() {
+    productsNumber.textContent = numProducts;
+    headerCartButton.classList.toggle('full', numProducts > 0);
+  }
+
+  productPlus.onclick = function() {
+    numProducts++;
+    updateCart();
+    console.log(numProducts);
+  };
+
+  productMinus.onclick = function() {
+    if (numProducts > 0) {
+      numProducts--;
+      updateCart();
+      console.log(numProducts);
+    }
+  };
+});
