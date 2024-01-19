af5cbade9301f0f42e997db11cbffaf068ca629e
Anastasia <an.gorskova.bg@gmail.com>
2024-01-19 04:07:22 +0100

    Обновляет шапку

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,39 +1,27 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let headerCartButton = document.querySelector('.header-cart-button');
-  let productsNumber = document.querySelector('.products-number');
-  let productPlus = document.querySelector('.plus');
-  let productMinus = document.querySelector('.minus');
-  let numProducts = parseInt(productsNumber.textContent);
+document.addEventListener('DOMContentLoaded', function () {
+  // Кнопка поиска
+  var searchOpenButton = document.getElementById("searchOpenButton");
+  var searchActivationButton = document.getElementById("searchActivationButton");
+  var searchInput = document.getElementById("search");
+  var wishlistButtonWrapper = document.querySelector(".user-communication__button-wrapper--wishlist");
+  var cartButtonWrapper = document.querySelector(".user-communication__button-wrapper--cart");
 
-  function updateCart() {
-    productsNumber.textContent = numProducts;
-    headerCartButton.classList.toggle('full', numProducts > 0);
-  }
+  searchOpenButton.addEventListener('click', function () {
+    searchActivationButton.classList.toggle("visually-hidden");
+    searchInput.classList.toggle("visually-hidden");
+    wishlistButtonWrapper.classList.toggle("visually-hidden");
+    cartButtonWrapper.classList.toggle("visually-hidden");
+    searchOpenButton.classList.toggle("icon-button__user-communication--search-button-opened");
+  });
 
-  productPlus.onclick = function() {
-    numProducts++;
-    updateCart();
-    console.log(numProducts);
-  };
+  // Кнопка меню
+  const menuButton = document.querySelector('.main-header__menu-button');
+  const navigationList = document.querySelector('.main-nav__list');
 
-  productMinus.onclick = function() {
-    if (numProducts > 0) {
-      numProducts--;
-      updateCart();
-      console.log(numProducts);
-    }
-  };
-
-  //кликанье по категории фильтра
-  let buttonContainers = document.querySelectorAll('.fieldset-openable');
-
-  for (let buttonContainer of buttonContainers) {
-    buttonContainer.onclick = function () {
-        buttonContainer.classList.toggle('opened');
-        buttonContainer.classList.toggle('closed');
-    };
-  };
-
-
-  //Изменение названия и цену продукта по селекту
+  menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('icon-button__header-menu-button--opened');
+    menuButton.classList.toggle('icon-button__header-menu-button--closed');
+    navigationList.classList.toggle('main-nav__list--mobile-opened');
+    navigationList.classList.toggle('main-nav__list--mobile-closed');
+  });
 });
