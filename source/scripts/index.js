f2b3b3068087316642a42adfaff6e6fba46d3ace
Anastasia <an.gorskova.bg@gmail.com>
2024-01-22 02:08:28 +0100

    Обновляет мобильное меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -24,4 +24,23 @@ document.addEventListener('DOMContentLoaded', () => {
     navigationList.classList.toggle('main-nav__list--mobile-opened');
     navigationList.classList.toggle('main-nav__list--mobile-closed');
   });
+
+  // кнопка подкатегории
+  const listDownButtons = document.querySelectorAll('.main-nav__main-list-text--list-down');
+  const subList = document.querySelector('.main-nav__sublist');
+  const mainList = document.querySelector('.main-nav__main-list');
+
+  listDownButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+
+      // Переключаем классы для subList
+      subList.classList.toggle('main-nav__sublist--closed');
+      subList.classList.toggle('main-nav__sublist--opened');
+
+      // Переключаем классы для mainList
+      mainList.classList.toggle('main-nav__main-list--mobile-closed');
+      mainList.classList.toggle('main-nav__main-list--mobile-opened');
+    });
+  });
 });
