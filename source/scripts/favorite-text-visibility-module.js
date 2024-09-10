ad4aa89e7212fed3bbd6b1e418a596af3b8af820
Anastasia <an.gorskova.bg@gmail.com>
2024-09-10 02:14:29 +0200

    исправляет ошибку скрытия пустого сообщения

--- source/scripts/favorite-text-visibility-module.js
+++ source/scripts/favorite-text-visibility-module.js
@@ -1,6 +1,6 @@
 export function toggleFavoriteMessage() {
   const favoriteList = document.getElementById('favoriteList');
-  const favoriteMessage = document.querySelector('.catalog-favorite__message');
+  const favoriteMessage = document.querySelector('.no-articles');
 
   if (favoriteList && favoriteMessage) {
     if (favoriteList.children.length > 0) {
